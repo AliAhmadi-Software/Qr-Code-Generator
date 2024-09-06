24229372fb5045e1ab181dab3f5ca7d122018050
Ali Ahmadi <aliahmadi79sh@gmail.com>
2024-09-06 09:14:00 +0330

    Final Commit For Push

---
+++ Qr Code Generator/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Qr_Code_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.qr = new Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeImgControl();
+            ((System.ComponentModel.ISupportInitialize)(this.qr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtText.Location = new System.Drawing.Point(38, 244);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtText.Size = new System.Drawing.Size(325, 116);
+            this.txtText.TabIndex = 1;
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(38, 386);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnEncode.TabIndex = 2;
+            this.btnEncode.Text = "Ecode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(288, 386);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 23);
+            this.btnDecode.TabIndex = 3;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(157, 386);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // qr
+            // 
+            this.qr.ErrorCorrectLevel = Gma.QrCodeNet.Encoding.ErrorCorrectionLevel.M;
+            this.qr.Image = ((System.Drawing.Image)(resources.GetObject("qr.Image")));
+            this.qr.Location = new System.Drawing.Point(74, 12);
+            this.qr.Name = "qr";
+            this.qr.QuietZoneModule = Gma.QrCodeNet.Encoding.Windows.Render.QuietZoneModules.Two;
+            this.qr.Size = new System.Drawing.Size(231, 211);
+            this.qr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.qr.TabIndex = 5;
+            this.qr.TabStop = false;
+            this.qr.Text = "qrCodeImgControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 456);
+            this.Controls.Add(this.qr);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Code";
+            ((System.ComponentModel.ISupportInitialize)(this.qr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnSave;
+        private Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeImgControl qr;
+    }
+}
+
---
+++ Qr Code Generator/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.QrCodeNet.Encoding;
+using ZXing;
+
+namespace Qr_Code_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            qr.Text = txtText.Text;
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                qr.Image = Bitmap.FromFile(open.FileName);
+                IBarcodeReader Reader = new BarcodeReader();
+                var BarcodeBitmap = (Bitmap)Bitmap.FromFile(open.FileName);
+                var Result = Reader.Decode(BarcodeBitmap);
+                txtText.Text = Result.Text;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Save = new SaveFileDialog();
+            Save.Filter = " Jpeg File(*.jpg)|*.jpg | Bitmap File (*.bmp) |*.bmp | Png File )(*.png)|*.png";
+            if (Save.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap b = new Bitmap(qr.Image, 500, 500);
+                b.Save(Save.FileName);
+
+            }
+        }
+    }
+}
